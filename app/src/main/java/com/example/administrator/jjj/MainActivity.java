175ecd040ed7d9c39c3250d3af2afe53982d8026
272303249 <272303249@qq.com>
2016-08-30 08:50:06 +0800

    更改了显示内容

--- app/src/main/java/com/example/administrator/jjj/MainActivity.java
+++ app/src/main/java/com/example/administrator/jjj/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.administrator.jjj;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textview= (TextView) findViewById(R.id.textview);
+        textview.setText("genggaile");
+
     }
 }
